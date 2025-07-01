fdc456482029e85163a86411de1b0cd9abe8a8b6
LFriede <gORDon_vdLg@gmx.de>
2025-07-01 22:27:18 +0200

    updated deploy script

--- deploy.bat
+++ deploy.bat
@@ -4,13 +4,21 @@ mkdir deploy
 
 copy _res\connect.wav deploy
 copy _res\disconnect.wav deploy
-copy _res\net_properties.dll deploy
+if exist _res\net_properties.dll (
+  copy _res\net_properties.dll deploy
+) else (
+  echo "net_properties.dll must be added by hand for now, exiting."
+  goto :exit
+)
 copy Win32\Release\ip_notification.exe deploy
 copy README.md deploy
+copy CHANGELOG.md deploy
+copy LICENSE deploy
 
 cd deploy
 upx ip_notification.exe
 
-7za a release-bin.zip *.*
+7za a release-bin.zip *
 
+:exit
 pause
